9eaa9181593a90ca170a4f5e16762e7da6366232
Sham Muthayyan <smuthayy@codeaurora.org>
2013-11-10 21:58:01 +0530

    [ipq806x] Add AP145 board support
    CRs Fixed: 574297
    
    Change-Id: Iceec55ec73298102d9993780291e732d30daa730
    Signed-off-by: Sham Muthayyan <smuthayy@codeaurora.org>

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1111,6 +1111,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_IPQ806X_DB149_1XX    4811
 #define MACH_TYPE_IPQ806X_DB147        4703
 #define MACH_TYPE_IPQ806X_AP148        4704
+#define MACH_TYPE_IPQ806X_AP145        4810
 #define MACH_TYPE_OMAP5_SEVM           3777
 
 #ifdef CONFIG_ARCH_EBSA110
--- board/qcom/ipq806x_cdp/ipq806x_board_param.h
+++ board/qcom/ipq806x_cdp/ipq806x_board_param.h
@@ -443,6 +443,50 @@ board_ipq806x_params_t board_params[] = {
 		}
 
 	},
+	{
+		.machid = MACH_TYPE_IPQ806X_AP145,
+		.ddr_size = (256 << 20),
+		.uart_gsbi = GSBI_4,
+		.uart_gsbi_base = UART_GSBI4_BASE,
+		.uart_dm_base = UART4_DM_BASE,
+		.mnd_value = { 12, 625, 313 },
+		.gmac_gpio_count = ARRAY_SIZE(gmac1_gpio),
+		.gmac_gpio = gmac1_gpio,
+		.gmac_cfg = {
+			gmac_board_cfg(1, 1, RGMII, S17_RGMII0_1_8V,
+					S17_RGMII1_1_8V, 0, 1, 4),
+			gmac_board_cfg(2, 1, SGMII, S17_RGMII0_1_8V,
+					S17_RGMII1_1_8V, 0, 4, 0, 1, 2, 3),
+			gmac_board_cfg_invalid(),
+			gmac_board_cfg_invalid(),
+		},
+		.flashdesc = NAND_NOR,
+		.flash_param = {
+			.mode =	NOR_SPI_MODE_0,
+			.bus_number = GSBI_BUS_5,
+			.chip_select = SPI_CS_0,
+			.vendor = SPI_NOR_FLASH_VENDOR_SPANSION,
+		},
+		.dbg_uart_gpio = {
+			{
+				.gpio = 10,
+				.func = 1,
+				.dir = GPIO_OUTPUT,
+				.pull = GPIO_NO_PULL,
+				.drvstr = GPIO_12MA,
+				.enable = GPIO_DISABLE
+			},
+			{
+				.gpio = 11,
+				.func = 1,
+				.dir = GPIO_INPUT,
+				.pull = GPIO_NO_PULL,
+				.drvstr = GPIO_12MA,
+				.enable = GPIO_DISABLE
+			},
+		}
+
+	},
 };
 
 #define NUM_IPQ806X_BOARDS	ARRAY_SIZE(board_params)
