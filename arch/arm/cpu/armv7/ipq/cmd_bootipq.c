186abf47bfdfa9a867b97904ef44d93e05752ffe
Ravindranath Thiyagarajan <rthiyaga@codeaurora.org>
2014-09-09 18:59:09 +0530

    ipq806x: bootmbn cmd for booting kernel in mbn format
    The kernel image is converted to mbn format by
    adding 40 byte header above the uImage header during the build.
    
    The format conversion was done because Code signing tools expects
    the image to be in mbn format. mbn format of kernel image is going
    to be maintained for both secured boot and non secured boot.
    
    Added a new command do_bootmbn to boot kernel in mbn format.
    
    Change-Id: I611370dc2374f3cf0897c2454db1aa7b65758ad7
    Signed-off-by: Ravindranath Thiyagarajan <rthiyaga@codeaurora.org>

--- arch/arm/cpu/armv7/ipq/cmd_bootipq.c
+++ arch/arm/cpu/armv7/ipq/cmd_bootipq.c
@@ -180,8 +180,6 @@ static int inline do_dumpipq_data()
 	return CMD_RET_SUCCESS;
 }
 
-
-#ifdef CONFIG_IPQ_LINUX_SECBOOT
 static int switch_ce_channel_buf(unsigned int channel_id)
 {
 	int ret;
@@ -195,12 +193,11 @@ static int switch_ce_channel_buf(unsigned int channel_id)
 
 	return ret;
 }
-#endif
 
 /**
- * Load the NSS images and Kernel image and transfer control to kernel
+ * Load the Kernel image in mbn format and transfer control to kernel.
  */
-static int do_bootipq(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
+static int do_bootmbn(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
 #ifdef CONFIG_IPQ_LOAD_NSS_FW
 	char bootargs[IH_NMLEN+32];
@@ -372,7 +369,6 @@ static int do_bootipq(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 		kernel_img_info.kernel_load_size =  sfi->hlos.size;
 	}
 
-#ifdef CONFIG_IPQ_LINUX_SECBOOT
 	request += sizeof(mbn_header_t);
 
 	/* This sys call will switch the CE1 channel to register usage */
@@ -397,8 +393,181 @@ static int do_bootipq(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 
 	if (ret)
 		return CMD_RET_FAILURE;
-#endif
+
 	snprintf(runcmd, sizeof(runcmd), "bootm 0x%x\n", request);
+
+	if (debug)
+		printf(runcmd);
+
+#ifdef CONFIG_IPQ_MMC
+	board_mmc_deinit();
+#endif
+
+	if (run_command(runcmd, 0) != CMD_RET_SUCCESS)
+		return CMD_RET_FAILURE;
+
+	return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(bootmbn, 2, 0, do_bootmbn,
+	   "bootmbn from flash device",
+	   "bootmbn [debug] - Load image(s) and boots the kernel\n");
+
+/**
+ * Load the NSS images and Kernel image and transfer control to kernel
+ */
+static int do_bootipq(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
+{
+#ifdef CONFIG_IPQ_LOAD_NSS_FW
+	char bootargs[IH_NMLEN+32];
+#endif
+	char runcmd[256];
+	int nandid = 0, ret;
+
+#ifdef CONFIG_IPQ_MMC
+	block_dev_desc_t *blk_dev = mmc_get_dev(host->dev_num);
+	disk_partition_t disk_info;
+#endif
+
+#ifdef CONFIG_IPQ_APPSBL_DLOAD
+	unsigned long * dmagic1 = (unsigned long *) 0x2A03F000;
+	unsigned long * dmagic2 = (unsigned long *) 0x2A03F004;
+#endif
+
+	if (argc == 2 && strncmp(argv[1], "debug", 5) == 0)
+		debug = 1;
+
+#ifdef CONFIG_IPQ_APPSBL_DLOAD
+	/* check if we are in download mode */
+	if (*dmagic1 == 0xE47B337D && *dmagic2 == 0x0501CAB0) {
+		/* clear the magic and run the dump command */
+		*dmagic1 = 0;
+		*dmagic2 = 0;
+		uint64_t etime = get_timer_masked() + (10 * CONFIG_SYS_HZ);
+
+		printf("\nCrashdump magic found."
+			"\nHit any key within 10s to stop dump activity...");
+		while (!tstc()) {       /* while no incoming data */
+			if (get_timer_masked() >= etime) {
+				if (do_dumpipq_data() == CMD_RET_FAILURE)
+					return CMD_RET_FAILURE;
+				break;
+			}
+		}
+
+		/* reset the system, some images might not be loaded
+		 * when crashmagic is found
+		 */
+		run_command("reset", 0);
+	}
+#endif
+
+#ifdef CONFIG_IPQ806X_PCI
+	board_pci_deinit();
+#endif /* CONFIG_IPQ806X_PCI */
+
+	if ((ret = set_fs_bootargs(&ipq_fs_on_nand)))
+		return ret;
+
+	/* check the smem info to see which flash used for booting */
+	if (sfi->flash_type == SMEM_BOOT_SPI_FLASH) {
+		nandid = 1;
+		if (debug) {
+			printf("Using nand device 1\n");
+		}
+		run_command("nand device 1", 0);
+	} else if (sfi->flash_type == SMEM_BOOT_NAND_FLASH) {
+		if (debug) {
+			printf("Using nand device 0\n");
+		}
+	} else if (sfi->flash_type == SMEM_BOOT_MMC_FLASH) {
+		if (debug) {
+			printf("Using MMC device\n");
+		}
+	} else {
+		printf("Unsupported BOOT flash type\n");
+		return -1;
+	}
+
+#ifdef CONFIG_IPQ_LOAD_NSS_FW
+	/* check the smem info to see whether the partition size is valid.
+	 * refer board/qcom/ipq806x_cdp/ipq806x_cdp.c:ipq_get_part_details
+	 * for more details
+	 */
+	if (sfi->nss[0].size != 0xBAD0FF5E) {
+		sprintf(runcmd, "nand read 0x%x 0x%llx 0x%llx",
+				CONFIG_SYS_LOAD_ADDR,
+				sfi->nss[0].offset, sfi->nss[0].size);
+
+		if (load_nss_img(runcmd, bootargs, sizeof(bootargs), 0)
+				!= CMD_RET_SUCCESS)
+			return CMD_RET_FAILURE;
+
+		if (getenv("nssbootargs0") == NULL)
+			setenv("nssbootargs0", bootargs);
+
+		run_command("setenv bootargs ${bootargs} ${nssbootargs0}", 0);
+	}
+
+	if (sfi->nss[1].size != 0xBAD0FF5E) {
+		sprintf(runcmd, "nand read 0x%x 0x%llx 0x%llx",
+				CONFIG_SYS_LOAD_ADDR,
+				sfi->nss[1].offset, sfi->nss[1].size);
+
+		if (load_nss_img(runcmd, bootargs, sizeof(bootargs), 1)
+				!= CMD_RET_SUCCESS)
+			return CMD_RET_FAILURE;
+
+		if (getenv("nssbootargs1") == NULL)
+			setenv("nssbootargs1", bootargs);
+
+		run_command("setenv bootargs ${bootargs} ${nssbootargs1}", 0);
+	}
+#endif /* CONFIG_IPQ_LOAD_NSS_FW */
+
+	if (debug) {
+		run_command("printenv bootargs", 0);
+		printf("Booting from flash\n");
+	}
+
+	if (ipq_fs_on_nand) {
+
+		/*
+		 * The kernel will be available inside a UBI volume
+		 */
+		snprintf(runcmd, sizeof(runcmd),
+			"set mtdids nand0=nand0 && "
+			"set mtdparts mtdparts=nand0:0x%llx@0x%llx(fs),${msmparts} && "
+			"ubi part fs && "
+			"ubi read 0x%x kernel && "
+			"bootm 0x%x\n", sfi->rootfs.size, sfi->rootfs.offset,
+				CONFIG_SYS_LOAD_ADDR, CONFIG_SYS_LOAD_ADDR);
+#ifdef CONFIG_IPQ_MMC
+	} else if (sfi->flash_type == SMEM_BOOT_MMC_FLASH) {
+		ret = find_part_efi(blk_dev, "0:HLOS", &disk_info);
+
+		snprintf(runcmd, sizeof(runcmd), "mmc read 0x%x 0x%X 0x%X",
+				CONFIG_SYS_LOAD_ADDR,
+				(uint)disk_info.start, (uint)disk_info.size);
+
+		if (run_command(runcmd, 0) != CMD_RET_SUCCESS)
+			return CMD_RET_FAILURE;
+
+		kernel_img_info.kernel_load_size = disk_info.size * disk_info.blksz;
+#endif
+	} else {
+
+		/*
+		 * Kernel is in a separate partition
+		 */
+		snprintf(runcmd, sizeof(runcmd),
+			/* NOR is treated as psuedo NAND */
+			"set mtdids nand1=nand1 && "
+			"set mtdparts mtdparts=nand1:${msmparts} && "
+			"set autostart yes;"
+			"nboot 0x%x %d 0x%llx", CONFIG_SYS_LOAD_ADDR,
+				nandid, sfi->hlos.offset);
+	}
 
 	if (debug)
 		printf(runcmd);
