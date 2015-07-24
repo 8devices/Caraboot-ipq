df2776e649c5e95cfb048cf2e259dcb81b3724a6
nrameg <nrameg@codeaurora.org>
2015-07-24 13:24:06 +0530

    ipq40xx: Remove dtb entry
    Change-Id: If8472daccfda53eec1e14f583aa2ca07abaccd28
    Signed-off-by: nrameg <nrameg@codeaurora.org>

--- board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
+++ board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
@@ -260,7 +260,6 @@ void qca_get_part_details(void)
 	struct { char *name; qca_part_entry_t *part; } entries[] = {
 		{ "0:HLOS", &smem->hlos },
 		{ "rootfs", &smem->rootfs },
-		{ "0:DTB", &smem->dtb },
 	};
 
 	for (i = 0; i < ARRAY_SIZE(entries); i++) {
