e67698715d766d2ae74be0127e924b81841c9857
Akila N <akilan@codeaurora.org>
2016-02-09 11:46:26 +0530

    ipq40xx: AP.DK07.1-C1 u-boot config support
    Change-Id: I6379ee4fbbc0f858971f9858d07a28aa835c87ff
    Signed-off-by: Akila N <akilan@codeaurora.org>

--- drivers/net/ipq40xx/ipq40xx_ess_sw.c
+++ drivers/net/ipq40xx/ipq40xx_ess_sw.c
@@ -74,6 +74,7 @@ int ipq40xx_ess_sw_init(ipq40xx_edma_board_cfg_t *cfg)
 	case MACH_TYPE_IPQ40XX_AP_DK04_1_C2:
 	case MACH_TYPE_IPQ40XX_AP_DK04_1_C3:
 	case MACH_TYPE_IPQ40XX_AP_DK06_1_C1:
+	case MACH_TYPE_IPQ40XX_AP_DK07_1_C1:
 
 		ipq40xx_ess_sw_wr(S17_P0LOOKUP_CTRL_REG, 0x140000);
 		ipq40xx_ess_sw_wr(S17_P1LOOKUP_CTRL_REG, 0x140000);
