c6cc859ca4477c8ea682b81b6c76002cff38274b
Aditya Kumar Patra S <apatr@codeaurora.org>
2016-06-30 20:51:09 +0530

    qca:ipq806x: Adding board support code for ipq806x
    Added dts and initial board support for ipq806x.
    
    Change-Id: I33992413905a0399ea3d6dfbd606c8e7ec30b6bf
    Signed-off-by: Aditya Kumar Patra S <apatr@codeaurora.org>

---
+++ board/qca/ipq806x/ipq806x.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <common.h>
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <environment.h>
+
+#include "ipq806x.h"
+#include "../common/qca_common.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+qca_mmc mmc_host;
+
+void enable_caches(void)
+{
+	icache_enable();
+}
+
+void disable_caches(void)
+{
+	icache_disable();
+}
+
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+	return 0;
+}
+
+unsigned long timer_read_counter(void)
+{
+	return 0;
+}
+
+void reset_cpu(unsigned long a)
+{
+	while(1);
+}
+void emmc_clock_config(int mode)
+{
+	/* TODO: To be filled */
+}
+int board_mmc_init(bd_t *bis)
+{
+	int ret;
+
+	mmc_host.base = MSM_SDC1_BASE;
+	mmc_host.clk_mode = MMC_IDENTIFY_MODE;
+	emmc_clock_config(mmc_host.clk_mode);
+
+	ret = qca_mmc_init(bis, &mmc_host);
+
+	return ret;
+}
+
