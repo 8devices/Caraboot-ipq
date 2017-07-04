7195fa74b24c1d86c228ae787b0d1a513fc3ce08
Sham Muthayyan <smuthayy@codeaurora.org>
2017-07-04 17:40:59 +0530

    ipq807x: Added the 2nd UART clocks
    Change-Id: I057848f175778c08187fad1991d5ae00422c0793
    Signed-off-by: Sham Muthayyan <smuthayy@codeaurora.org>

--- board/qca/arm/ipq807x/ipq807x.c
+++ board/qca/arm/ipq807x/ipq807x.c
@@ -14,6 +14,7 @@
 #include <common.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
+#include <asm/errno.h>
 #include <environment.h>
 #include <asm/arch-qca-common/qca_common.h>
 #include <asm/arch-qca-common/qpic_nand.h>
@@ -53,9 +54,77 @@ struct dumpinfo_t dumpinfo[] = {
 };
 int dump_entries = ARRAY_SIZE(dumpinfo);
 
+void uart2_configure_mux(void)
+{
+	unsigned long cfg_rcgr;
+
+	cfg_rcgr = readl(GCC_BLSP1_UART2_APPS_CFG_RCGR);
+	/* Clear mode, src sel, src div */
+	cfg_rcgr &= ~(GCC_UART_CFG_RCGR_MODE_MASK |
+			GCC_UART_CFG_RCGR_SRCSEL_MASK |
+			GCC_UART_CFG_RCGR_SRCDIV_MASK);
+
+	cfg_rcgr |= ((UART2_RCGR_SRC_SEL << GCC_UART_CFG_RCGR_SRCSEL_SHIFT)
+			& GCC_UART_CFG_RCGR_SRCSEL_MASK);
+
+	cfg_rcgr |= ((UART2_RCGR_SRC_DIV << GCC_UART_CFG_RCGR_SRCDIV_SHIFT)
+			& GCC_UART_CFG_RCGR_SRCDIV_MASK);
+
+	cfg_rcgr |= ((UART2_RCGR_MODE << GCC_UART_CFG_RCGR_MODE_SHIFT)
+			& GCC_UART_CFG_RCGR_MODE_MASK);
+
+	writel(cfg_rcgr, GCC_BLSP1_UART2_APPS_CFG_RCGR);
+}
+
+void uart2_set_rate_mnd(unsigned int m,
+			unsigned int n, unsigned int two_d)
+{
+	writel(m, GCC_BLSP1_UART2_APPS_M);
+	writel(NOT_N_MINUS_M(n, m), GCC_BLSP1_UART2_APPS_N);
+	writel(NOT_2D(two_d), GCC_BLSP1_UART2_APPS_D);
+}
+
+int uart2_trigger_update(void)
+{
+	unsigned long cmd_rcgr;
+	int timeout = 0;
+
+	cmd_rcgr = readl(GCC_BLSP1_UART2_APPS_CMD_RCGR);
+	cmd_rcgr |= UART2_CMD_RCGR_UPDATE;
+	writel(cmd_rcgr, GCC_BLSP1_UART2_APPS_CMD_RCGR);
+
+	while (readl(GCC_BLSP1_UART2_APPS_CMD_RCGR) & UART2_CMD_RCGR_UPDATE) {
+		if (timeout++ >= CLOCK_UPDATE_TIMEOUT_US) {
+			printf("Timeout waiting for UART2 clock update\n");
+			return -ETIMEDOUT;
+		}
+		udelay(1);
+	}
+	cmd_rcgr = readl(GCC_BLSP1_UART2_APPS_CMD_RCGR);
+	return 0;
+}
+
+void uart2_toggle_clock(void)
+{
+	unsigned long cbcr_val;
+
+	cbcr_val = readl(GCC_BLSP1_UART2_APPS_CBCR);
+	cbcr_val |= UART2_CBCR_CLK_ENABLE;
+	writel(cbcr_val, GCC_BLSP1_UART2_APPS_CBCR);
+}
+
+void uart2_clock_config(unsigned int m,
+			unsigned int n, unsigned int two_d)
+{
+	uart2_configure_mux();
+	uart2_set_rate_mnd(m, n, two_d);
+	uart2_trigger_update();
+	uart2_toggle_clock();
+}
+
 void qca_serial_init(struct ipq_serial_platdata *plat)
 {
-	int node;
+	int node, uart2_node;
 
 	writel(1, GCC_BLSP1_UART1_APPS_CBCR);
 
@@ -65,6 +134,17 @@ void qca_serial_init(struct ipq_serial_platdata *plat)
 		return;
 	}
 
+	if (plat->port_id == 1) {
+		uart2_node = fdt_path_offset(gd->fdt_blob, "uart2");
+		if (uart2_node < 0) {
+			printf("Could not find uart2 node\n");
+			return;
+		}
+		node = fdt_subnode_offset(gd->fdt_blob,
+				uart2_node, "serial_gpio");
+		uart2_clock_config(plat->m_value, plat->n_value, plat->d_value);
+		writel(1, GCC_BLSP1_UART2_APPS_CBCR);
+	}
 	qca_gpio_init(node);
 }
 
