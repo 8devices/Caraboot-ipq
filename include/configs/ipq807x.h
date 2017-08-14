71c93c6ee1590c5eddd33c26afd2e5baf3e1c20b
Venkat Raju Sana <vrsana@codeaurora.org>
2017-08-14 13:37:43 -0700

    ipq807x: Increase size of  CONFIG_SYS_MALLOC_LEN
    UBI is memory greedy and needs more malloc area
    while accessing  above 256M of Flash.
    
    Change-Id: I2521eedaf483e104bcb37886003085ed74c8bf0c
    Signed-off-by: Venkat Raju Sana <vrsana@codeaurora.org>

--- include/configs/ipq807x.h
+++ include/configs/ipq807x.h
@@ -116,7 +116,7 @@ extern loff_t board_env_size;
 #define CONFIG_ENV_SIZE			CONFIG_ENV_SIZE_MAX
 #define CONFIG_ENV_RANGE		board_env_range
 #define CONFIG_ENV_SIZE_MAX		(256 << 10) /* 256 KB */
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE_MAX + (512 << 10))
+#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE_MAX + (1024 << 10))
 
 #define CONFIG_ENV_IS_IN_NAND		1
 
