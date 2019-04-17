066d99d8bba085fd638ec6952c241b4738805362
Mantas Pucka <mantas@8devices.com>
2019-04-17 18:09:45 +0300

    ipq40xx: enable CPU cache for kernel booting
    Significant speed-up for hash checking and LZMA decompression
    Disable cache when exiting bootm code, in case boot fails
    Having cache enabled prevents ETH and SPI NOR drivers from working

--- common/cmd_bootm.c
+++ common/cmd_bootm.c
@@ -594,6 +594,7 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		relocated = 1;
 	}
 #endif
+	dcache_enable();
 
 	/* determine if we have a sub command */
 	if (argc > 1) {
@@ -608,12 +609,19 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		 *
 		 * Right now we assume the first arg should never be '-'
 		 */
-		if ((*endp != 0) && (*endp != ':') && (*endp != '#'))
-			return do_bootm_subcommand(cmdtp, flag, argc, argv);
+		if ((*endp != 0) && (*endp != ':') && (*endp != '#')) {
+			ret = do_bootm_subcommand(cmdtp, flag, argc, argv);
+			dcache_disable();
+			return ret;
+		}
+
+
 	}
 
-	if (bootm_start(cmdtp, flag, argc, argv))
+	if (bootm_start(cmdtp, flag, argc, argv)) {
+		dcache_disable();
 		return 1;
+	}
 
 	/*
 	 * We have reached the point of no return: we are going to
@@ -659,6 +667,7 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 			if (iflag)
 				enable_interrupts();
 			bootstage_error(BOOTSTAGE_ID_DECOMP_UNIMPL);
+			dcache_disable();
 			return 1;
 		}
 	}
@@ -670,6 +679,7 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 			enable_interrupts();
 		/* This may return when 'autostart' is 'no' */
 		bootm_start_standalone(iflag, argc, argv);
+		dcache_disable();
 		return 0;
 	}
 
@@ -688,6 +698,7 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		printf("ERROR: booting os '%s' (%d) is not supported\n",
 			genimg_get_os_name(images.os.os), images.os.os);
 		bootstage_error(BOOTSTAGE_ID_CHECK_BOOT_OS);
+		dcache_disable();
 		return 1;
 	}
 
@@ -701,6 +712,7 @@ int do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #endif
 	do_reset(cmdtp, flag, argc, argv);
 
+	dcache_disable();
 	return 1;
 }
 
