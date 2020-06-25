499f538fac14dc945efec64a6cca9a371d002c02
Gokul Sriram Palanisamy <gokulsri@codeaurora.org>
2020-06-25 12:11:34 +0530

    qca: Made machid and soc_hw_version as default env
    Made machid, soc_hw_version, soc_version_minor
    and soc_version_major as default env across all
    qca arm platforms.
    
    Signed-off-by: Gokul Sriram Palanisamy <gokulsri@codeaurora.org>
    Change-Id: I0f284dac705d929f7b6588bc8fe4eaf17916b4b6

--- board/qca/arm/ipq807x/ipq807x.c
+++ board/qca/arm/ipq807x/ipq807x.c
@@ -1790,20 +1790,6 @@ void run_tzt(void *address)
 	execute_tzt(address);
 }
 
-void set_platform_specific_default_env(void)
-{
-	uint32_t soc_ver_major, soc_ver_minor, soc_version;
-	int ret;
-
-	ret = ipq_smem_get_socinfo_version((uint32_t *)&soc_version);
-	if (!ret) {
-		soc_ver_major = SOCINFO_VERSION_MAJOR(soc_version);
-		soc_ver_minor = SOCINFO_VERSION_MINOR(soc_version);
-		setenv_ulong("soc_version_major", (unsigned long)soc_ver_major);
-		setenv_ulong("soc_version_minor", (unsigned long)soc_ver_minor);
-	}
-}
-
 void sdi_disable(void)
 {
 	qca_scm_sdi();
