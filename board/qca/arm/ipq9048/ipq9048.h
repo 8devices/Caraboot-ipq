46ad9dc3792640eaec3a35e224b163a14526e6b3
anusha <anusharao@codeaurora.org>
2020-12-22 08:38:24 +0530

    qca: ipq9048: Enable SMEM_VERSION_C support
    Added config to enable smem version C.
    Updated SYS_TEXT_BASE and SMEM_BASE addresses.
    
    Change-Id: If6cdb1dffc3ad37e0eba833cf1538487a8f3045c
    Signed-off-by: anusha <anusharao@codeaurora.org>

--- board/qca/arm/ipq9048/ipq9048.h
+++ board/qca/arm/ipq9048/ipq9048.h
@@ -26,6 +26,62 @@
 #define KERNEL_AUTH_CMD		0x13
 #define SCM_CMD_SEC_AUTH	0x1F
 
+#ifdef CONFIG_SMEM_VERSION_C
+#define RAM_PART_NAME_LENGTH 16
+
+#define SECONDARY_CORE_STACKSZ (8 * 1024)
+#define CPU_POWER_DOWN (1 << 16)
+
+#define ARM_PSCI_TZ_FN_BASE		0x84000000
+#define ARM_PSCI_TZ_FN(n)		(ARM_PSCI_TZ_FN_BASE + (n))
+
+#define ARM_PSCI_TZ_FN_CPU_OFF		ARM_PSCI_TZ_FN(2)
+#define ARM_PSCI_TZ_FN_CPU_ON		ARM_PSCI_TZ_FN(3)
+#define ARM_PSCI_TZ_FN_AFFINITY_INFO	ARM_PSCI_TZ_FN(4)
+
+unsigned int __invoke_psci_fn_smc(unsigned int, unsigned int,
+					 unsigned int, unsigned int);
+
+/**
+ * Number of RAM partition entries which are usable by APPS.
+ */
+#define RAM_NUM_PART_ENTRIES 32
+struct ram_partition_entry
+{
+	char name[RAM_PART_NAME_LENGTH];  /**< Partition name, unused for now */
+	u64 start_address;             /**< Partition start address in RAM */
+	u64 length;                    /**< Partition length in RAM in Bytes */
+	u32 partition_attribute;       /**< Partition attribute */
+	u32 partition_category;        /**< Partition category */
+	u32 partition_domain;          /**< Partition domain */
+	u32 partition_type;            /**< Partition type */
+	u32 num_partitions;            /**< Number of partitions on device */
+	u32 hw_info;                   /**< hw information such as type and frequency */
+	u8 highest_bank_bit;           /**< Highest bit corresponding to a bank */
+	u8 reserve0;                   /**< Reserved for future use */
+	u8 reserve1;                   /**< Reserved for future use */
+	u8 reserve2;                   /**< Reserved for future use */
+	u32 reserved5;                 /**< Reserved for future use */
+	u64 available_length;          /**< Available Partition length in RAM in Bytes */
+};
+
+struct usable_ram_partition_table
+{
+	u32 magic1;          /**< Magic number to identify valid RAM partition table */
+	u32 magic2;          /**< Magic number to identify valid RAM partition table */
+	u32 version;         /**< Version number to track structure definition changes
+	                             and maintain backward compatibilities */
+	u32 reserved1;       /**< Reserved for future use */
+
+	u32 num_partitions;  /**< Number of RAM partition table entries */
+
+	u32 reserved2;       /** < Added for 8 bytes alignment of header */
+
+	/** RAM partition table entries */
+	struct ram_partition_entry ram_part_entry[RAM_NUM_PART_ENTRIES];
+};
+#endif
+
 struct smem_ram_ptn {
 	char name[16];
 	unsigned long long start;
@@ -62,6 +118,7 @@ struct smem_ram_ptable {
 } __attribute__ ((__packed__));
 
 int smem_ram_ptable_init(struct smem_ram_ptable *smem_ram_ptable);
+int smem_ram_ptable_init_v2(struct usable_ram_partition_table *usable_ram_partition_table);
 void reset_crashdump(void);
 void reset_board(void);
 
